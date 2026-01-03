fbf23e4ea1d6fe42abd614ea396f6b01243d643a
mzubair12147 <muhammadzubair12147@gmail.com>
2026-01-03 16:13:12 +0500

    fixed a bug

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import { Header } from "@/components/header"
+import { Hero } from "@/components/hero"
+import { DetailedServices } from "@/components/detailed-services"
+import { About } from "@/components/about"
+import { CTA } from "@/components/cta"
+import { Footer } from "@/components/footer"
+import { EmailContact } from "@/components/email-contact"
+
+export default function Home() {
+    return (
+        <main className="min-h-screen">
+            <Header />
+            <Hero />
+            <DetailedServices />
+            <About />
+            <EmailContact />
+            <Footer />
+        </main>
+    )
+}
